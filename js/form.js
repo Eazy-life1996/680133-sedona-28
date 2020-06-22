7333292287dbcd2595f32a26e46935c20f171cd6
Eazy-life1996 <eazylifemusic@gmail.com>
2020-06-22 16:43:28 +0600

    добавил правки, оживил вформу с помощью JS и добавил интерактивную карту

---
+++ js/form.js
@@ -0,0 +1,47 @@
+let buttonShow = document.querySelector(".button");
+let formBlock = document.querySelector(".form-search");
+let arrivalDate = formBlock.querySelector(".arrival-user");
+let departureDate = formBlock.querySelector(".departure-user");
+let formValidation = formBlock.querySelector(".form-validation");
+let adultUsers = formBlock.querySelector(".user-adult");
+let childUsers = formBlock.querySelector(".user-child");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+localStorage.setItem("login", adultUsers.value);
+localStorage.setItem("login", childUsers.value);
+
+buttonShow.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    formBlock.classList.toggle("form-search-none");
+    formBlock.classList.toggle("form-search");
+    arrivalDate.focus();
+    formBlock.classList.remove("form-error");
+})
+
+formValidation.addEventListener("submit", function (evt) {
+    if (!arrivalDate.value || !departureDate.value) {
+        evt.preventDefault();
+        formBlock.classList.remove("form-error");
+        formBlock.offsetWidth = formBlock.offsetWidth;
+        formBlock.classList.add("form-error");
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (formBlock.classList.contains("form-search")) {
+        evt.preventDefault();
+        formBlock.classList.remove(".form-error");
+        formBlock.classList.toggle("form-search-none");
+        formBlock.classList.toggle("form-search");
+    }
+  }
+});
